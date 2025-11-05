0d01cb5fe60be22d573794787a38629aedd19ede
Abraham <153450541+MaybeAbe@users.noreply.github.com>
2025-11-04 19:10:15 -0500

    Load and filter data for SWD, ELL, and Econ Status
    This script loads data from an Excel file and filters relevant columns and rows for three categories: SWD, ELL, and Economic Status.

--- MTH440_RScript.R
+++ MTH440_RScript.R
@@ -1,1 +1,34 @@
+#Loading Data
+library(dplyr)
+library(readxl) #When importanting data, insert path of dataset
+
+relevant_columns = c("District","Grade", "Year","Category","# Level 1","# Level 2","# Level 3+4")
+SWD <- read_excel("Documents/03_district-math-results-2013-2023-(public).xlsx", 
+                  sheet = "SWD")
+
+SWD <- SWD[,relevant_columns]#Drop Columns
+SWD <- SWD %>%  #Drop Rows not with all grade
+  filter(Grade == "All Grades")
+
+
+ELL <- read_excel("Documents/03_district-math-results-2013-2023-(public).xlsx", 
+                  sheet = "ELL")
+
+ELL <- ELL[,relevant_columns]#Drop Columns
+ELL <- ELL %>%  #Drop Rows not with all grade
+  filter(Grade == "All Grades")
+
+
+Econ_Status <- read_excel("Documents/03_district-math-results-2013-2023-(public).xlsx", 
+                  sheet = "Econ Status")
+
+Econ_Status <- Econ_Status[,relevant_columns]#Drop Columns
+Econ_Status <- Econ_Status %>%  #Drop Rows not with all grade
+  filter(Grade == "All Grades")
+
+##
+
+
+
+
 
